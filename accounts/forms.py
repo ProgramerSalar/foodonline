cc6352bdba7b416a5148b8a52167171b8ce96267
ProgramerSalar <manishkumar60708090@gmail.com>
2023-05-29 13:21:28 +0530

    prepare address fields

--- accounts/forms.py
+++ accounts/forms.py
@@ -24,6 +24,7 @@ class UserForm(forms.ModelForm):
             
             
 class UserProfileForm(forms.ModelForm):
+    address = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Start typing...', 'required': 'required'}))
     profile_picture = forms.FileField(widget=forms.FileInput(attrs={'class':'btn-btn-info'}), validators=[allow_only_images_validator])
     cover_photo = forms.FileField(widget=forms.FileInput(attrs={'class':'btn-btn-info'}), validators=[allow_only_images_validator])
     
@@ -31,7 +32,10 @@ class UserProfileForm(forms.ModelForm):
     # longitude = forms.CharField(widget=forms.TextInput(attrs={'readonly':'readonly'}))
     class Meta:
         model = UserProfile
-        fields = ['profile_picture', 'cover_photo', 'address_line_1', 'address_line_2', 'country', 'state', 'pin_code', 'latitude', 'longitude', 'city']
+        fields = ['profile_picture', 'cover_photo', 'address', 'country', 'state', 'pin_code', 'latitude', 'longitude', 'city']
+
+
+
 
     def __init__(self, *args, **kwargs):
         super(UserProfileForm,self).__init__(*args, **kwargs)
--- accounts/models.py
+++ accounts/models.py
@@ -98,8 +98,7 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, blank=True, null=True, on_delete=models.CASCADE)
     profile_picture = models.ImageField(upload_to='users/profile_pictures', blank=True, null=True)
     cover_photo = models.ImageField(upload_to='users/cover_photos', blank=True , null=True)
-    address_line_1 = models.CharField(max_length=50, blank=True , null=True )
-    address_line_2 = models.CharField(max_length=50  )
+    address = models.CharField(max_length=250, blank=True , null=True )
     country  = models.CharField(max_length=15 , blank=True , null=True)
     state = models.CharField(max_length=50 , blank=True, null=True)
     city  = models.CharField(max_length=50 , blank=True , null=True)
@@ -109,8 +108,8 @@ class UserProfile(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
     
-    def full_address(self):
-        return f'{self.address_line_1},{self.address_line_2}'
+    # def full_address(self):
+    #     return f'{self.address_line_1},{self.address_line_2}'
 
 
     def __str__(self):
