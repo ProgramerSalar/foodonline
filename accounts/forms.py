58b1a431cd4cd3b428063df296815ed63136737b
ProgramerSalar <manishkumar60708090@gmail.com>
2023-05-29 12:21:10 +0530

    custom validator function allow only images

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,6 @@
 from django import forms 
 from .models import User, UserProfile
-
+from .validators import allow_only_images_validator
 
 
 class UserForm(forms.ModelForm):
@@ -24,8 +24,8 @@ class UserForm(forms.ModelForm):
             
             
 class UserProfileForm(forms.ModelForm):
-    profile_picture = forms.ImageField(widget=forms.FileInput(attrs={'class':'btn-btn-info'}))
-    cover_photo = forms.ImageField(widget=forms.FileInput(attrs={'class':'btn-btn-info'}))
+    profile_picture = forms.FileField(widget=forms.FileInput(attrs={'class':'btn-btn-info'}), validators=[allow_only_images_validator])
+    cover_photo = forms.FileField(widget=forms.FileInput(attrs={'class':'btn-btn-info'}), validators=[allow_only_images_validator])
     class Meta:
         model = UserProfile
         fields = ['profile_picture', 'cover_photo', 'address_line_1', 'address_line_2', 'country', 'state', 'pin_code', 'latitude', 'longitude', 'city']
---
+++ accounts/validators.py
@@ -0,0 +1,12 @@
+from django.core.exceptions import ValidationError
+import os 
+
+
+
+def allow_only_images_validator(value):
+    ext = os.path.splitext(value.name)[1]  # cover-image.jpg 
+    print(ext)
+    valid_extensions = ['.csv']
+    if not ext.lower() in valid_extensions:
+        raise ValidationError('Unsupported file extension. Allowed extensions: '+str(valid_extensions))
+    
