4604ab454eb72f56d726c3a0c1d38e3f210fcb53
ProgramerSalar <manishkumar60708090@gmail.com>
2023-05-30 16:27:34 +0530

    Add Test And Setup Admin Table

--- menu/admin.py
+++ menu/admin.py
@@ -3,6 +3,26 @@ from .models import Category, FoodItem
 
 
 # Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('category_name',)}
+    list_display = ('category_name','vendor', 'updated_at')
+    search_fields = ('category_name','vendor__vendor_name')
+    
 
-admin.site.register(Category)
-admin.site.register(FoodItem)
+
+class FoodItemAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('food_title',)}
+    list_display = ('food_title', 'category', 'vendor', 'price', 'is_available', 'updated_at')
+    search_fields = ('food_title', 'category__category_name', 'vendor__vendor_name', 'price')
+    list_filter = ('is_available',)
+    
+    
+
+
+
+
+
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(FoodItem, FoodItemAdmin)
