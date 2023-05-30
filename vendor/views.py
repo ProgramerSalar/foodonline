7f1ad4ddaf8f4e1e58e20d5e077451b6c9d9e61e
ProgramerSalar <manishkumar60708090@gmail.com>
2023-05-30 15:10:12 +0530

    fix the problem

--- vendor/views.py
+++ vendor/views.py
@@ -8,9 +8,14 @@ from django.contrib.auth.decorators import login_required, user_passes_test
 from accounts.views import check_role_vendor
 
 
+
+
+
+
+
+
+
 # Create your views here.
-
-
 @login_required(login_url='login')
 @user_passes_test(check_role_vendor)
 def vprofile(request):
