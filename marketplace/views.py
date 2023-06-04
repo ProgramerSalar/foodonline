0a2b834e3126d73ba2095b2ea8fcb1335d912cef
ProgramerSalar <manishkumar60708090@gmail.com>
2023-06-04 10:40:46 +0530

    vendor_detail.html page setup 3

--- marketplace/views.py
+++ marketplace/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from vendor.models import Vendor
-
 
 
 
@@ -19,6 +18,13 @@ def marketplace(request):
 
 
 
-
 def vendor_detail(request, vendor_slug):
-    return render(request, 'marketplace/vendor_detail.html')
+    vendor = get_object_or_404(Vendor, vendor_slug=vendor_slug)
+    context = {
+        'vendor':vendor,
+    }
+
+    return render(request, 'marketplace/vendor_detail.html',context)
+
+
+
