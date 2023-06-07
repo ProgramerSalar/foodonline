242d3e9d4b99ff6da74730ec843943eb46de8daf
ProgramerSalar <manishkumar60708090@gmail.com>
2023-06-07 13:28:38 +0530

    Decrease Cart Feature Edited

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -13,5 +13,6 @@ urlpatterns = [
 
     # add to cart 
     path('add_to_cart/<int:food_id>/', views.add_to_cart, name='add_to_cart'),
+    path('decrease_cart/<int:food_id>/', views.decrease_cart, name='decrease_cart'),
     
 ]
--- marketplace/views.py
+++ marketplace/views.py
@@ -82,3 +82,41 @@ def add_to_cart(request, food_id):
 
     else:
         return JsonResponse({'status': 'login_required', 'message': 'Please login to continue'})
+
+
+
+
+
+
+
+
+def decrease_cart(request,food_id):
+    if request.user.is_authenticated:
+        # if request.is_ajax():
+        
+        if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+
+            # Check if the food item exists
+            try:
+                fooditem = FoodItem.objects.get(id=food_id)
+                # Check if the user has already added that food to the cart
+                try:
+                    chkCart = Cart.objects.get(user=request.user, fooditem=fooditem)
+                    if chkCart.quantity >=1:
+                        # decrease the cart quantity
+                        chkCart.quantity -= 1
+                        chkCart.save()
+                    else:
+                        chkCart.delete()
+                        chkCart.quantity = 0
+
+                    return JsonResponse({'status': 'Success', 'cart_counter':get_cart_counter(request),'qty':chkCart.quantity})
+                except:
+                    return JsonResponse({'status': 'Failed', 'message': 'You do not have this item in your cart!'})
+            except:
+                return JsonResponse({'status': 'Failed', 'message': 'This food does not exist!'})
+        else:
+            return JsonResponse({'status': 'Failed', 'message': 'Invalid request!'})
+
+    else:
+        return JsonResponse({'status': 'login_required', 'message': 'Please login to continue'})
